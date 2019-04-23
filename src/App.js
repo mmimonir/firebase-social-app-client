98be328fcf3743a334283b2cbf78f8bf5997cc5e
mmimonir <monirdhk.it@gmail.com>
2019-04-24 00:35:33 +0600

    Class#15

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
+// Pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
+
+// Components
+import Nabvar from './components/Nabvar'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+})
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <div className="container">
+            <Router>
+              <Nabvar />
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am from signup</h1>
+      </div>
+    )
+  }
+}
+
+export default signup
